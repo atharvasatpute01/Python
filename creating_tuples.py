8dcb9d6002601416607c023d1a92ac6d45164bb8
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-08-19 17:57:37 +0530

    Add files via upload

---
+++ creating_tuples.py
@@ -0,0 +1,12 @@
+# Empty Tuples
+t1 = ()
+
+# Tuple with elements
+t2 = (10, 20, 30)
+
+# Parentheses are optional
+t3 = 1, 2, 3
+
+# Single-element tuple (comma is required)
+
+t4 = (5,)
--- tuple.py
+++ tuple.py
@@ -1,5 +1,12 @@
-# 6. Tuple (tuple)
+# In Python, a tuple is an ordered, immutable collection of items. Once a tuple is created, its elements cannot be changed, added, or removed.
 
-colors = ("Red", "Blue", "Green")
-print(colors)
-print(type(colors))
+
+
+
+# When should you use a tuple?
+
+# The data should not change (e.g., days of the week, RGB color values).
+
+# You want a lightweight, read-only collection.
+
+# You need a hashable sequence to use as a dictionary key or set element.
