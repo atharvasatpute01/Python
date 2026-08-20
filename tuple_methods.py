9b51e5e0933c5b46a3e288f97e2de85b4eb08c29
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-08-21 00:04:17 +0530

    Add files via upload

---
+++ tuple_methods.py
@@ -0,0 +1,6 @@
+# Tuples have only two built-in methods :
+
+t = (1, 2, 2, 3, 2)
+
+print(t.count(2))    # 3
+print(t.index(3))    # 3
---
+++ tuple_operations.py
@@ -0,0 +1,7 @@
+a = (1, 2)
+b = (3, 4)
+
+print(a + b)     # (1, 2, 3, 4)
+print(a * 3)     # (1, 2, 1, 2, 1, 2)
+print(2 in a)    # True
+print(len(a))    # 2
