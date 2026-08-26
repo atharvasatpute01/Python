58e05fd401f277c6baac5307e3c26fecaca9598e
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-08-27 00:14:33 +0530

    Add files via upload

---
+++ 4_loop_sets.py
@@ -0,0 +1,17 @@
+# 4. Loop Sets
+
+# Use a for loop :
+
+fruits = {"Apple", "Banana", "Mango", "Orange"}
+
+for fruit in fruits:
+    print(fruit)
+
+
+# Set with if
+
+numbers = {10, 20, 30, 40, 50}
+
+for number in numbers:
+    if number > 25:
+        print(number)
