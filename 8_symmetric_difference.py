5bd084e0d4d43f3b0a888424f416dcd4ccebbf9e
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-08-30 00:33:01 +0530

    Add files via upload

---
+++ 8_symmetric_difference.py
@@ -0,0 +1,19 @@
+# 8. Symmetric Difference
+
+# Returns elements that are in either set, but not in both.
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+result = a.symmetric_difference(b)
+
+print(result)
+
+
+
+
+# Using "^" :
+
+result = a ^ b
+
+print(result)
