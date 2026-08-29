bb8bb8a9750807bb6a435f78761ecc6a9b51df15
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-08-29 22:44:16 +0530

    Add files via upload

---
+++ 7_difference.py
@@ -0,0 +1,28 @@
+# 7. Difference
+
+# Returns elements present in the first set but not the second.
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+result = a.difference(b)
+
+print(result)
+
+
+
+
+# Using "-" :
+
+result = a - b
+
+print(result)
+
+
+
+
+# Reverse difference
+
+result = b - a
+
+print(result)
