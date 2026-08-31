81fcbb6c607028b06d472f1189a0f2d31690ac04
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-01 00:06:01 +0530

    Add files via upload

---
+++ 10_set_methods.py
@@ -0,0 +1,69 @@
+# 10. Set Methods
+
+# add()
+# Add one item.
+
+
+
+# update()
+# Add multiple items.
+
+
+
+# remove() 
+# Remove item; error if absent.
+
+
+
+# discard()
+# Remove item; no error if absent.
+
+
+
+# pop()
+# Remove arbitrary item.
+
+
+
+# clear()
+# Remove all items.
+
+
+
+# union()
+# Combine sets.
+
+
+
+# intersection()
+# Common elements.
+
+
+
+# difference()
+# Elements only in first set.
+
+
+
+# symmetric_difference
+# Elements in either but not both.
+
+
+
+# issubset()
+# Check subset.
+
+
+
+# issuperset()
+# Check superset.
+
+
+
+# isdisjoint()
+# Check whether sets have no common elements.
+
+
+
+# copy()
+# Create a copy.
