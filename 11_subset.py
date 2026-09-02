8fc1759e89d2f0c508c0f73ed182f137bde1d049
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-02 09:26:52 +0530

    Add files via upload

---
+++ 11_subset.py
@@ -0,0 +1,19 @@
+# 11. Subset
+
+# A set is a subset if every element of it exists in another set.
+
+a = {1, 2}
+b = {1, 2, 3, 4}
+
+print(a.issubset(b))
+
+# Output:
+
+# True
+
+
+
+
+# You can also use :
+
+print(a <= b)
