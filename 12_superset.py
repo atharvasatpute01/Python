cc7cfe5932111a15be69265ac691d54219e14601
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-03 10:01:25 +0530

    Add files via upload

---
+++ 12_superset.py
@@ -0,0 +1,19 @@
+# 12. Superset
+
+# A set is a superset if it contains every element of another set.
+
+a = {1, 2, 3, 4}
+b = {1, 2}
+
+print(a.issuperset(b))
+
+# Output:
+
+# True
+
+
+
+
+# Using:
+
+print(a >= b)
