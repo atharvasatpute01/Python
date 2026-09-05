eca01ca1a630d8c1d99eec109ba876d9628d8bad
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-05 09:42:26 +0530

    Add files via upload

---
+++ 14_remove_duplicates_using_sets.py
@@ -0,0 +1,9 @@
+# 14. Remove Duplicates Using Sets
+
+# One of the most useful applications of sets is removing duplicates from a list.
+
+numbers = [10, 20, 10, 30, 20, 40, 30]
+
+unique_numbers = set(numbers)
+
+print(unique_numbers)
