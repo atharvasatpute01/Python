d84c0b5b58b59b12e6caf5f740da6272a74fb8d5
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-07 22:50:08 +0530

    Add files via upload

---
+++ python_while_loop.py
@@ -0,0 +1,31 @@
+# Python while Loop 
+
+# A while loop repeatedly executes code as long as a condition is True.
+
+# Syntax :
+
+# while condition:
+#     statement
+
+
+# Example :
+
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+
+# How it works
+# count = 1
+#     ↓
+# count <= 5 ?
+#     ↓
+#    YES
+#     ↓
+# print(count)
+#     ↓
+# count += 1
+#     ↓
+# # Check condition again
