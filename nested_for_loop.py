c9f88825d7f9de3ef1403e244e4d32912d9b14d3
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-09 03:54:34 +0530

    Add files via upload

---
+++ nested_for_loop.py
@@ -0,0 +1,10 @@
+# Nested for Loop
+
+# A nested for loop means putting one for loop inside another for loop.
+
+# Basic Syntax
+# for outer in sequence:
+#     for inner in sequence:
+#         statement
+
+# The inner loop completes all its iterations for every iteration of the outer loop.
