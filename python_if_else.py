7e18cd828c09ec2990977d37927967b6fda01187
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-10 10:17:39 +0530

    Add files via upload

---
+++ python_if_else.py
@@ -0,0 +1,61 @@
+# What is if?
+
+# if is used when we want Python to make a decision.
+
+# Basic Syntax :
+
+# if condition:
+#     statement
+
+
+# Example :
+
+age = 20
+
+if age >= 18:
+    print("You are an adult")
+
+
+
+
+# if else
+
+# Use else when there are two possible outcomes.
+
+age = 15
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+
+
+
+# if elif else
+
+# Use elif when there are multiple conditions.
+
+marks = 75
+
+if marks >= 90:
+    print("Grade A+")
+elif marks >= 75:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 40:
+    print("Grade C")
+else:
+    print("Fail")
+
+
+
+
+# Important Operators :
+# >     Greater than
+# <     Less than
+# >=    Greater than or equal
+# <=    Less than or equal
+# ==    Equal to
+# !=    Not equal to
