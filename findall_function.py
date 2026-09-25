290e0b952331ac9543132fc570a24299e0dfa500
atharvasatpute01 <atharvasatputeaiml0101@gmail.com>
2026-09-25 09:40:00 +0530

    Add files via upload

---
+++ findall_function.py
@@ -0,0 +1,16 @@
+# The findall() Function
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.findall("ai", txt)
+# print(x)
+
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
